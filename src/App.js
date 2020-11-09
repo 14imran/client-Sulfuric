55bd60d6dfe24ad93f6840b7dae2d35dcf084bdc
14imran <14.imran.mohammed@gmail.com>
2020-11-09 20:57:11 +0530

    added frontend {auth}

--- src/App.js
+++ src/App.js
@@ -1,25 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {react,useState} from 'react'
+import Signup from "./components/Signup";
+import Signin from "./components/Signin";
+import axios from 'axios'
+import {Route, Switch,Redirect} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.css'
 
-function App() {
+import React from 'react'
+import MyNav from "./components/MyNav";
+
+export default function App(props) {
+  let [loggedInUser, setloggedInUser] = useState()
+
+  const handleSignUp = (e) => {
+        e.preventDefault()
+        const {username, email, password} = e.target
+    
+        axios.post(`http://localhost:5000/api/signup` , {
+          username: username.value, 
+          email: email.value, 
+          password: password.value
+        }, {withCredentials: true})
+          .then((response) => {
+    
+          setloggedInUser(()=>{
+            loggedInUser= response.data;
+          })
+          // props.history.push('/');
+
+              })}
+
+            const  handleSignIn = (e) => {
+                e.preventDefault()
+                e.preventDefault()
+                const {email, password} = e.target
+            
+                axios.post(`http://localhost:5000/api/signin` , {
+                  email: email.value, 
+                  password: password.value
+                }, {withCredentials: true})
+                  .then((response) => {
+                    setloggedInUser({
+                      loggedInUser: response.data
+                    })
+                  })
+                  .catch((er) => {
+                    setloggedInUser({
+                        errorMessage: er.response.data.error
+                      })
+                  })
+              }
+        
+        
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     
+      <MyNav />
+       <Switch>
+
+   <Route path="/sign-up" render={(routeProps) => {
+            return <Signup onSignUp={handleSignUp} {...routeProps} />
+          }}/>
+
+<Route path="/sign-in" render={(routeProps) => {
+            return <Signin onSignIn={handleSignIn} {...routeProps} />
+          }}/>
+
+  </Switch>
     </div>
-  );
+  )
 }
 
-export default App;
+// export default  function App(props) {
+// const [loggedInUser, setloggedInUser] = useState()
+
+
+
+//   const handleSignUp = (e) => {
+//     e.preventDefault()
+//     const {username, email, password} = e.target
+
+//     axios.post(`http://localhost:5000/api/signup` , {
+//       username: username.value, 
+//       email: email.value, 
+//       password: password.value
+//     }, {withCredentials: true})
+//     .then((response) => {
+
+//       setloggedInUser(()=>{
+//         loggedInUser= response.data;
+//         // props.history.push('/');
+//      })
+//     }
+     
+
+        
+      
+
+//   return (
+//   <div>
+//   <Switch>
+//   {/* <Signin /> */}
+//   <Route path="/sign-up" render={(routeProps) => {
+//             return <Signup onSignUp={handleSignUp} {...routeProps} />
+//           }}/>
+//   </Switch>
+//   </div>
+  
+//   )
+//   }
+
+        
+
---
+++ src/components/MyNav.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {Navbar, Nav} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
+
+function MyNav(props) {
+
+    let buttonStyle = {marginLeft: '10px'}
+
+    return (
+        <Navbar bg="light" expand="lg">
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Link to="/">Todos</Link>
+                    <Link style={buttonStyle} to="/add-form">Add Todo</Link>
+                    {
+                        props.loggedInUser ? (
+                            <button style={buttonStyle}  onClick={props.onLogout}>Logout</button>
+                        ) : (
+                            <>
+                            <Link style={buttonStyle}  to="/sign-in">Sign In</Link>
+                            <Link style={buttonStyle}to="/sign-up">Sign Up</Link>
+                            </>
+                        )
+                    }
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+export default MyNav
